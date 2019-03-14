450847587743553cc77b2756b7ec3bb90b3f4ee8
Maglethong <maglethongspirr@gmail.com>
2019-03-14 13:07:33 +0100

    feat(main): added largest spread to main program

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,13 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.weather.IWeatherService;
+import de.exxcellent.challenge.weather.Weather;
+import de.exxcellent.challenge.weather.WeatherCsvService;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Optional;
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,13 +21,26 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        try {
+            // TODO => Linux compatible?
+            String csvPath = App.class
+                    .getResource("weather.csv")
+                    .getPath()
+                    .substring(1); // Remove starting '/'
 
-        // Your preparation code …
+            IWeatherService weatherService = new WeatherCsvService(csvPath);
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+            Optional<Weather> dayWithSmallestTempSpread = weatherService.getLargestSpread();
+            if (dayWithSmallestTempSpread.isPresent()) {
+                System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread.get().getDay());
+            } else {
+                System.out.print("Day not found!");
+            }
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+            String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
+            System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        } catch (Exception ignored) {
+            // TODO Treat exception
+        }
     }
 }
